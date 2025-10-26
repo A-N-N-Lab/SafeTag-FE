1e1d40090834896f0347e01def8df003c2db3f08
nadomola <siyun778@gmail.com>
2025-10-27 00:13:46 +0900

    :lipstick: MyPage 차량번호 수정 폼
    MyPage 차량번호 수정 폼

--- src/api/mypage.ts
+++ src/api/mypage.ts
@@ -1,26 +1,3 @@
-// import { api } from "./index";
-
-// export interface MyPageResponse {
-//   name: string;
-//   email: string;
-//   gender: string;
-//   phoneNum: string;
-//   birthDate: string;
-//   vehicleNumber: string;
-//   address: string;
-//   company: string;
-// }
-// export type MyPageUpdateRequest = Partial<
-//   Pick<
-//     MyPageResponse,
-//     "name" | "phoneNum" | "vehicleNumber" | "address" | "company"
-//   >
-// >;
-
-// export const getMyPage = () => api.get<MyPageResponse>("/api/mypage");
-// export const updateMyPage = (data: MyPageUpdateRequest) =>
-//   api.put("/api/mypage/edit", data);
-// src/api/mypage.ts
 import { api } from "./index";
 
 export interface MyPageRaw {
